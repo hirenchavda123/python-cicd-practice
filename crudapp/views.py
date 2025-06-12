cd51e02c0d837d4025c7c28011baf835b6310f30
HirenKoffeeKodes <hiren.koffeekodes@gmail.com>
2025-06-12 16:34:41 +0530

    first commit

---
+++ crudapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+def login_view(request):
+    if request.method == "POST":
+        data = request.POST
+        username = data.get("username")
+        password = data.get("password")
+        user = User.objects.filter(username=username)
+        if not user:
+            messages.error(request, f"Username:{username} does not exists.")
+            return render(request, "login.html")
+        if not check_password(password, user[0].password):
+            messages.error(request, "Password is incorrect.")
+            return render(request, "login.html")
+        context = {"message": f"User with username:{username} logged in successfully."}
+        return render(request, "home.html", context=context)
+    return render(request, "login.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        data = request.POST
+        email = data.get("email")
+        username = data.get("username")
+        password = data.get("password")
+        check_username = User.objects.filter(username=username).exists()
+        if check_username:
+            messages.error(
+                request,
+                f"User with username:{username} already exists. Try another username.",
+            )
+            return render(request, "signup.html")
+        check_email = User.objects.filter(email=email).exists()
+        if check_email:
+            messages.error(
+                request, f"User with email:{email} already exists. Try another email."
+            )
+            return render(request, "signup.html")
+        User.objects.create(
+            username=username, email=email, password=make_password(password)
+        )
+        context = {"message": f"User with username:{username} created successfully."}
+        return render(request, "login.html", context=context)
+    return render(request, "signup.html")
+
+
+@login_required(login_url='/login/')
+def home_view(request):
+    return render(request, "home.html")

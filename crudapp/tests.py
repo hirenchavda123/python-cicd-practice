0fab2ade66dfe3a435340d95751ed8d6e17ea4e4
HirenKoffeeKodes <hiren.koffeekodes@gmail.com>
2025-06-13 17:33:57 +0530

    create test cases

--- crudapp/tests.py
+++ crudapp/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+def add_funtion(a,b):
+    if type(a) is int:
+        print("a is integer")
+    else:
+        print("a is not integer")
+    
+    if type(b) is int:
+        print("b is integer")
+    else:
+        print("b is not integer")
 
-# Create your tests here.
+
+add_funtion("1",2)
